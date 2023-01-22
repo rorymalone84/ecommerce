91efe98a715d48e49300c0088f2aed4a533988fe
rory <rorymalone@live.com>
2023-01-22 14:55:07 +0000

    unauthorized user redirect

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -4,13 +4,18 @@ import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
+import NotFound from "../views/NotFound.vue";
 import Products from "../views/Products.vue";
+import store from "../store";
 
 const routes = [
     //app layout with children
     {
         path: "/app",
         name: "app",
+        meta: {
+            requiresAuth: true,
+        },
         component: AppLayout,
         children: [
             {
@@ -29,22 +34,51 @@ const routes = [
         path: "/login",
         name: "login",
         component: Login,
+        meta: {
+            requiresGuest: true,
+        },
     },
     {
         path: "/requestpassword",
         name: "requestPassword",
         component: RequestPassword,
+        meta: {
+            requiresGuest: true,
+        },
     },
     {
         path: "/resetpassword/:token",
         name: "resetPassword",
         component: ResetPassword,
+        meta: {
+            requiresGuest: true,
+        },
+    },
+    {
+        path: "/:pathMatch(.*)",
+        name: "notFound",
+        component: NotFound,
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+//unauthorized user redirect
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({
+            name: "login",
+        });
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({
+            name: "app.dashboard",
+        });
+    } else {
+        next();
+    }
 });
 
 export default router;
--- backend/src/store/index.js
+++ backend/src/store/index.js
@@ -2,7 +2,10 @@ import { createStore } from "vuex";
 
 const store = createStore({
     state: {
-        
+        user: {
+            token: null,
+            data: {},
+        },
     },
     getters: {},
     action: {},
